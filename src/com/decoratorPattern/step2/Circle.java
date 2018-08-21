e5a434b7d188d035212cc6b437db2f0e4641f3c9
yinxing <yinxing@dxy.cn>
2018-08-21 16:19:19 +0800

    [add]新增装饰者模式

---
+++ src/com/decoratorPattern/step2/Circle.java
@@ -0,0 +1,19 @@
+package com.decoratorPattern.step2;
+
+import com.decoratorPattern.step1.Shape;
+
+/**
+ * Created by yinxing on 2018/8/20.
+ */
+
+/**
+ * 步骤 2
+ * 创建实现接口的实体类。
+ */
+public class Circle implements Shape {
+
+    @Override
+    public void draw() {
+        System.out.println("Shape: Circle");
+    }
+}
