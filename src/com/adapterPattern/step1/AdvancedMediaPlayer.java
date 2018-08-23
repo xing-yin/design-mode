ed7fae37ad14d3bdc53918618485b3fe7418e30a
yinxing <yinxing@dxy.cn>
2018-08-23 11:53:35 +0800

    [add]新增适配器模式

---
+++ src/com/adapterPattern/step1/AdvancedMediaPlayer.java
@@ -0,0 +1,11 @@
+package com.adapterPattern.step1;
+
+/**
+ * Created by yinxing on 2018/8/23.
+ */
+public interface AdvancedMediaPlayer {
+
+    public void playVlc(String fileName);
+
+    public void playMp4(String fileName);
+}
---
+++ src/com/adapterPattern/step2/Mp4Player.java
@@ -0,0 +1,24 @@
+package com.adapterPattern.step2;
+
+import com.adapterPattern.step1.AdvancedMediaPlayer;
+
+/**
+ * Created by yinxing on 2018/8/23.
+ */
+
+/**
+ * 步骤 2
+ * 创建实现了 AdvancedMediaPlayer 接口的实体类
+ */
+public class Mp4Player implements AdvancedMediaPlayer {
+
+    @Override
+    public void playVlc(String fileName) {
+        //do nothing
+    }
+
+    @Override
+    public void playMp4(String fileName) {
+        System.out.println("Playing Mp4 file. Name:" + fileName);
+    }
+}
