1a57f45aa083e645939fbfa8fbdcd5a201c6b91d
yinxing <yinxing@dxy.cn>
2018-08-28 15:26:21 +0800

    [add]新增迭代器模式

---
+++ src/com/iteratorPattern/step1/Iterator.java
@@ -0,0 +1,16 @@
+package com.iteratorPattern.step1;
+
+/**
+ * Created by yinxing on 2018/8/28.
+ */
+
+/**
+ * 步骤 1
+ * 创建接口
+ */
+public interface Iterator {
+
+    public boolean hasNext();
+
+    public Object next();
+}
---
+++ src/com/iteratorPattern/step2/NameRepository.java
@@ -0,0 +1,44 @@
+package com.iteratorPattern.step2;
+
+/**
+ * Created by yinxing on 2018/8/28.
+ */
+
+import com.iteratorPattern.step1.Container;
+import com.iteratorPattern.step1.Iterator;
+
+/**
+ * 步骤 2
+ * 创建实现了 Container 接口的实体类。该类有实现了 Iterator 接口的内部类 NameIterator。
+ */
+public class NameRepository implements Container {
+
+    public String names[] = {"a", "b", "c", "d"};
+
+    @Override
+    public Iterator getIterator() {
+        return new NameIterator();
+    }
+
+    private class NameIterator implements Iterator {
+
+        int index;
+
+        @Override
+        public boolean hasNext() {
+            if (index < names.length) {
+                return true;
+            }
+            return false;
+        }
+
+        @Override
+        public Object next() {
+            if (this.hasNext()) {
+                return names[index++];
+            }
+            return null;
+        }
+    }
+
+}
