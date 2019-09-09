b7b4e270ea8cdbc600ea120e2ad2ec052dc93ec7
yinxing <yinxing@dxy.cn>
2019-09-09 13:39:09 +0800

    [add]新增装饰者模式

--- src/com/runoob/decoratorPattern/step1/Shape.java
+++ src/com/runoob/decoratorPattern/step1/Shape.java
@@ -1,6 +1,7 @@
 package com.runoob.decoratorPattern.step1;
 
 /**
+ * 创建一个接口
  * Created by yinxing on 2018/7/30.
  */
 public interface Shape {
--- src/com/runoob/decoratorPattern/step2/Rectangle.java
+++ src/com/runoob/decoratorPattern/step2/Rectangle.java
@@ -3,6 +3,7 @@ package com.runoob.decoratorPattern.step2;
 import com.runoob.decoratorPattern.step1.Shape;
 
 /**
+ * 创建实现接口的实体类
  * Created by yinxing on 2018/7/30.
  */
 public class Rectangle implements Shape {
